999dad1dc8f0c3c6aa5c648eafaeabad46157b24
Madhu <madhusraja22042002@gmail.com>
2024-12-11 22:45:01 +0530

    Hello

--- index.java
+++ index.java
@@ -1,1 +1,7 @@
-your java code
+//your java code
+public class index {
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
